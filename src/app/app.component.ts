0c0455a7a6aa153e5e4b1dcb99cb1655855b47d7
Rahimahameed <rahimahameed0222gmail.com>
2022-01-20 13:15:52 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name="Rhima"
+  age=22
+}
